50df3580148b267e412d16de31388882330ad919
Flaviotix <[flavionannini@gmail.com]>
2022-12-07 17:03:52 +0100

    main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void afficherBonjour()
+{
+    printf("Bonjour tout le monde");
+}
+
+void auRevoir()
+{
+    printf("Au revoir tout le monde");
+}
+
+int main(int argc, char const *argv[])
+{
+    afficherBonjour();
+    auRevoir();
+    return 0;
+}
